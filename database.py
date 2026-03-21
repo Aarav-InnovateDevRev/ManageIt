c8c3021f6e2b0c187dd579898f03607107af4b0b
Aarav-InnovateDevRev <aarav.techdragon@gmail.com>
2026-03-21 14:48:40 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,9 +1,15 @@
 import psycopg2
 import os
 
-DATABASE_URL = os.environ.get("DATABASE_URL")
-
 def get_db():
+    DATABASE_URL = os.environ.get("DATABASE_URL")
+    
+    if DATABASE_URL is None:
+        raise ValueError("DATABASE_URL environment variable is not set in Render!")
+    
+    # Fix protocol if needed (Supabase sometimes gives postgres://)
     if DATABASE_URL.startswith("postgres://"):
         DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+    
+    # Connect with SSL required (Supabase needs it)
     return psycopg2.connect(DATABASE_URL, sslmode="require")
