32940cfaebab980f09130dc7b7d2a334f835f365
Aarav-InnovateDevRev <aarav.techdragon@gmail.com>
2026-03-22 16:12:40 +0530

    Update database.py

--- database.py
+++ database.py
@@ -5,11 +5,11 @@ def get_db():
     DATABASE_URL = os.environ.get("DATABASE_URL")
     
     if DATABASE_URL is None:
-        raise ValueError("DATABASE_URL environment variable is missing!")
+        raise ValueError("DATABASE_URL environment variable is missing in Render!")
     
     # Fix protocol if Supabase gave postgres://
     if DATABASE_URL.startswith("postgres://"):
         DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
     
-    # Connect – SSL required for Supabase
+    # Connect with SSL required for Supabase
     return psycopg2.connect(DATABASE_URL, sslmode="require")
